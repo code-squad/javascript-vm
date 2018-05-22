f99d84cbf51f8c41f5a25db32a2b87feb6e4ff36
chany <lyhy0310@gmail.com>
2018-05-22 14:04:27 +0900

    [add] 나머지 모델 뷰 컨트롤러도 분리

---
+++ webVendingMachine/js/controller.js
@@ -0,0 +1,35 @@
+class VmController {
+  constructor(vendingMachine,wallet,vendingMachineView){
+    this.vendingMachine = vendingMachine;
+    this.wallet = wallet;
+    this.vendingMachineView = vendingMachineView;
+  }
+  on(eventName, data){
+    this[eventName](data)
+  }
+  useMoney(data){
+    this.wallet.useMoney(data)
+    this.insertMoney(data)
+  }
+  reRenderWallet(data){
+    this.vendingMachineView.reRenderWallet(data);
+  }
+  insertMoney(data){
+    this.vendingMachine.insertMoney(data);
+  }
+  reRenderVendingMachineMoney(data){
+    this.vendingMachineView.reRenderVendingMachineMoney(data);
+  }
+  displayCanBuyList(money){
+    this.vendingMachineView.displayCanBuyList(money);
+  }
+  reRenderLog(latestHistorys){
+    this.vendingMachineView.reRenderLog(latestHistorys);
+  }
+  handleSelectButtonClicked(buttonText){
+    this.vendingMachine.logSelection('selected',buttonText)
+  }
+  startTimer(time){
+    this.vendingMachineView.startTimer(time);
+  }
+}
---
+++ webVendingMachine/js/view.js
@@ -0,0 +1,109 @@
+class VendingMachineView {
+  constructor(){
+    this.snackListEl = this.getSearched('.snack-list')
+    this.selectButtonsEl = this.getSearched('.number-buttons')
+    this.moneyButtonListEl = this.getSearched('.money-button-list')
+    this.myTotalMoneyEl = this.getSearched('.total-my-assets .money')
+    this.insertedMoneyEl = this.getSearched('.diplay-inserted-money .money')
+    this.displayLogEl = this.getSearched('.display-log-box')
+    this.timer = this.getSearched('.time')
+    this.controller = null;
+    this.actions = {
+      'insertMoney': (data)=> `<p class="log">${data}원이 입력되었습니다</p>`,
+      'selected': (data)=> `<p class="selected-text">${data} 번</p>`,
+    }
+  }
+  getSearched(selector, target=document){
+    return target.querySelector(selector);
+  }
+  getSearchedAll(selector, target=document){
+    return target.querySelectorAll(selector);
+  }
+  updateText(el,updateText){
+    return el.innerText = updateText;
+  }
+  getMessageByType(type, data){
+    return this.actions[type](data)
+  }
+  initRender(template, data){
+    const {snackTemplate, selectButtonTemplate, walletMoneyButtonTemplate} = template
+    const {snackList, buttonTextList, myMoney} = data
+    this.snackListEl.insertAdjacentHTML('beforeend', snackTemplate(snackList))
+    this.selectButtonsEl.insertAdjacentHTML('beforeend', selectButtonTemplate(buttonTextList))
+    this.moneyButtonListEl.insertAdjacentHTML('beforeend', walletMoneyButtonTemplate(myMoney))
+    this.myTotalMoneyEl.innerText = Object.keys(myMoney).reduce((ac,money)=> {
+      return ac+=Number(money)*myMoney[money]
+    },0)
+  }
+  handleMoneyButtonClicked({target}){
+    if(!target.localName==="button") return;
+    const moneyCountEl = target.nextElementSibling
+    const moneyCount =  Number(moneyCountEl.dataset.count)
+    if(!moneyCount) return;
+    const eventData = {
+      money: target.dataset.money,
+      moneyCountEl,
+      totalMoneyEl: this.myTotalMoneyEl,
+      insertedMoneyEl: this.insertedMoneyEl,
+    };
+    this.emit('useMoney', eventData)
+  }
+  emit(eventName, data){
+    this.controller.on(eventName, data);
+  }
+  bindEvents(){
+    this.selectButtonsEl.addEventListener('click', e =>this.handleSelectButtonClicked(e));
+    this.moneyButtonListEl.addEventListener('click', e =>this.handleMoneyButtonClicked(e));
+    return this;
+  }
+  getCanBuyList(money){
+    const eachSnacks = this.getSearchedAll(`[data-id]`, this.snackListEl);
+    return Array.prototype.filter.call(eachSnacks,(snackEl)=>{
+      const price = snackEl.dataset.price
+      if(price<=money) return snackEl;
+    })
+  }
+  addClassElList(list, className){
+    list.forEach(el=> el.classList.add(className))
+  }
+  displayCanBuyList(money){
+    const canBuyList = this.getCanBuyList(money);
+    this.addClassElList(canBuyList, 'red')
+  }
+  makeLogTemplate(latestHistorys){
+     return latestHistorys.reduce(
+      (ac,{type, data})=>{
+        return ac+=this.getMessageByType(type,data)
+      },``);
+  }
+  reRenderLog(latestHistorys){
+    const latestMsgTemplate = this.makeLogTemplate(latestHistorys);
+    this.displayLogEl.innerHTML = latestMsgTemplate;
+    this.displayLogEl.lastElementChild.classList.add('now')
+  }
+  displaySelectedButtonNumber(){
+
+  }
+  handleSelectButtonClicked(e){
+    // 버튼 텍스트 보내기  
+    const buttonText = e.target.innerText    
+    this.emit('handleSelectButtonClicked',buttonText)
+  }
+  reRenderVendingMachineMoney(data){
+   this.updateText(this.insertedMoneyEl, `${data.insertedMoney}`)
+  }
+  reRenderWallet(data){
+    this.updateText(data.moneyCountEl, `${data.moneyCount}개`);
+    data.moneyCountEl.setAttribute('data-count',data.moneyCount);
+    this.updateText(data.totalMoneyEl, data.totalMoney);
+  }
+  startTimer(time){
+    let initTime = time;
+    this.timer.innerText = initTime;
+    const intervalId = setInterval(()=>{
+      if(initTime===0) return clearTimeout(intervalId);
+        initTime-=1
+        this.timer.innerText = initTime; 
+      },1000)
+  }
+}
