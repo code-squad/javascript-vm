0625493b5494fe17b1309eeec46e62fdf883bfd0
Log91 <peter1636@naver.com>
2018-06-27 15:36:03 +0900

    refactor: template literal을 이용해 html랜더링 로직 간소화 외 1개
    1. template literal 이용해서 로직을 간결하게 변경
    2. css에서 li태그 이용하던 것을 class를 부여하는 것으로 변경

--- js/beverageDisplay.js
+++ js/beverageDisplay.js
@@ -2,27 +2,26 @@ class Display {
   constructor(beverage) {
     this.beverage = beverage;
   }
-  displayBeverage() {
+  createBeverageContainer() {
     const beverageList = document.createElement('ul');
     beverageList.className = 'beverageListContainer';
     document.querySelector('.beverageDisplay').appendChild(beverageList);
+  }
+  displayBeverage() {
+    this.createBeverageContainer();
+    let beverageListHTML = '';
     this.beverage.forEach((ele, idx) => {
-      let item = document.createElement('li');
-      let list = beverageList.appendChild(item);
-      const beverageContainer = document.createElement('div');
-      beverageContainer.className = 'beverageContainer';
-      list.appendChild(beverageContainer);
-      let beverageName = document.createElement('div');
-      beverageName.className = 'beverageName';
-      let beveragePrice = document.createElement('div');
-      beveragePrice.className = 'beveragePrice';
-      beverageName.innerHTML = ele.name;
-      beveragePrice.innerHTML = `${idx + 1}. ${ele.price}`;
-      beverageContainer.appendChild(beverageName);
-      beverageContainer.appendChild(beveragePrice);
+      beverageListHTML +=
+        `<li class="beverageItem">
+        <div class="beverageContainer">
+        <div class="beverageName">${ele.name}</div>
+        <div class="beveragePrice">${idx+1}. ${ele.price}</div>
+        </div>
+        </li>`;
     })
+    document.querySelector('.beverageListContainer').innerHTML = beverageListHTML;
   }
 }
 
-const displayItem = new Display(beverage);
-displayItem.displayBeverage();
+const displayBeverage = new Display(beverage);
+displayBeverage.displayBeverage();
