da3725dbcc004535aa975c2caadc3ed17a92b017
imdonguk <com8157@naver.com>
2018-07-23 22:57:08 +0900

    Add : 동전을 넣었을 때 view가 동작하는 InsertCoinView클래스 추가

---
+++ js/InsertCoinView.js
@@ -0,0 +1,32 @@
+class InsertCoinView{
+    constructor(){
+        this.insertCoinHandler = null;
+        this.coinCount = {
+            100 : 0,
+            500 : 0,
+            1000 : 0,
+            5000 : 0,
+            10000 : 0
+        }
+        this.clickBtn();
+    }
+    clickBtn(){
+        const btns = document.querySelector(".wallet > ul");
+        btns.addEventListener("click", e=>{
+            const target = e.target;
+            if(target.className === "basic-button insert-coin-button"){
+                const coin = target.dataset.coin;
+                console.log("넣은 동전 : " + coin);
+                this.plusCoinCount(coin);
+                target.nextElementSibling.innerText = this.coinCount[coin] + "개";
+                this.insertCoinHandler(+coin);
+            }
+        })
+    }
+    plusCoinCount(coin){
+        for(let key in this.coinCount){
+            if(coin === key)this.coinCount[key]++;
+        }
+    }
+}
+
