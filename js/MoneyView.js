e16dbfb728a591aa3d076e95636f87f7f8720f0d
imdonguk <com8157@naver.com>
2018-07-28 19:59:07 +0900

    Add : inputMoney가 변할 때 event를 발생시키기 위해 inputMoneyView함수에 inputMoneyHandler함수호출

--- js/MoneyView.js
+++ js/MoneyView.js
@@ -2,11 +2,13 @@ class MoneyView{
     constructor(){
         this.yourMoney = null;
         this.inputMoney = null;
+        this.inputMoneyHandler = null;
     }
     walletView(){
         document.querySelector(".your-money").innerText = this.yourMoney + "원";
     }
     inputMoneyView(){
         document.querySelector(".input-money").innerText = this.inputMoney + "원";
+        this.inputMoneyHandler();
     }
 }
