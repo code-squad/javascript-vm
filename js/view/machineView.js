74f6ba03a7e77926d9243c2c7ddc811bd56da106
Log91 <peter1636@naver.com>
2018-07-31 15:16:36 +0900

    modify: 모르게 전역에서 받아오던 machineModel을 클래스 내에서 받아오게 변경

--- js/view/machineView.js
+++ js/view/machineView.js
@@ -3,62 +3,62 @@
   초기 디스플레이, 이벤트 시에 화면 변화를 담당한다
 */
 class MachineView {
-    constructor(machineModel) {
-        this.machineModel = machineModel;
-        this.clickItemNumberButtonHandler = null;
-        this.displayItem(machineModel.itemList);
-        this.displayTotalInsertedMoney(machineModel.totalInsertedMoney);
-        this.clickItemNumberButton();
-    }
+  constructor(machineModel) {
+    this.machineModel = machineModel;
+    this.clickItemNumberButtonHandler = null;
+    this.displayItem(this.machineModel.itemList);
+    this.displayTotalInsertedMoney(this.machineModel.totalInsertedMoney);
+    this.clickItemNumberButton();
+  }
 
-    displayItem(itemList) {
-        this.renderItem(itemList);
-    }
-    clickItemNumberButton() {
-        const itemNumberList = document.querySelectorAll('.coin_button_item');
-        itemNumberList.forEach(v => {
-            v.addEventListener('click', ({ target }) => {
-                this.clickItemNumberButtonHandler(target);
-            })
-        })
-    }
-    displayInsertLog(insertedMoney) {
-        const logList = document.querySelector('.log_list');
-        const logItem = document.createElement('li');
-        logItem.innerHTML =
-            `<li class="log_item">
+  displayItem(itemList) {
+    this.renderItem(itemList);
+  }
+  clickItemNumberButton() {
+    const itemNumberList = document.querySelectorAll('.coin_button_item');
+    itemNumberList.forEach(v => {
+      v.addEventListener('click', ({ target }) => {
+        this.clickItemNumberButtonHandler(target);
+      })
+    })
+  }
+  displayInsertLog(insertedMoney) {
+    const logList = document.querySelector('.log_list');
+    const logItem = document.createElement('li');
+    logItem.innerHTML =
+      `<li class="log_item">
               <span>${Util.numberWithCommas(insertedMoney)}원이 투입됐습니다</span>
             </li>`
-        logList.insertAdjacentHTML('afterBegin', logItem.innerHTML);
-    }
-    renderItem(itemList) {
-        let processedItemList = itemList.reduce((acc, ele, idx) => {
-            acc +=
-                `<li class="item">
+    logList.insertAdjacentHTML('afterBegin', logItem.innerHTML);
+  }
+  renderItem(itemList) {
+    let processedItemList = itemList.reduce((acc, ele, idx) => {
+      acc +=
+        `<li class="item">
       <div class="item_container">
       <div class="item_name">${ele.name}</div>
       <div data-price=${ele.price} class="item_price">${idx + 1}. ${ele.price}</div>
       </div>
       </li>`;
-            return acc;
-        }, '');
-        document.querySelector('.item_list_container').innerHTML = processedItemList;
+      return acc;
+    }, '');
+    document.querySelector('.item_list_container').innerHTML = processedItemList;
+  }
+  displayTotalInsertedMoney(totalInsertedMoney) {
+    let currentCoin = document.querySelector('.current_coin');
+    currentCoin.innerHTML = `${Util.numberWithCommas(totalInsertedMoney)}원`;
+  }
+  displayAvailableItem() {
+    const itemList = document.querySelectorAll('.item_price');
+    const totalInsertedMoney = this.machineModel.totalInsertedMoney;
+    const availableItemClass = 'available_item';
+    for (let ele of itemList) {
+      if (ele.getAttribute('data-price') <= totalInsertedMoney) {
+        if (!ele.classList.contains(availableItemClass)) ele.classList.add(availableItemClass);
+      }
     }
-    displayTotalInsertedMoney(totalInsertedMoney) {
-        let currentCoin = document.querySelector('.current_coin');
-        currentCoin.innerHTML = `${Util.numberWithCommas(totalInsertedMoney)}원`;
-    }
-    displayAvailableItem() {
-        const itemList = document.querySelectorAll('.item_price');
-        const totalInsertedMoney = this.machineModel.totalInsertedMoney;
-        const availableItemClass = 'available_item';
-        for (let ele of itemList) {
-            if (ele.getAttribute('data-price') <= totalInsertedMoney) {
-                if (!ele.classList.contains(availableItemClass)) ele.classList.add(availableItemClass);
-            }
-        }
-    }
-    rerender(totalInsertedMoney) {
-        this.displayTotalInsertedMoney(totalInsertedMoney)
-    }
+  }
+  rerender(totalInsertedMoney) {
+    this.displayTotalInsertedMoney(totalInsertedMoney)
+  }
 }
