cfa9dcb45ca7efdc2ccf2922e37edf1ae1ee45f5
imdonguk <com8157@naver.com>
2018-08-01 14:32:02 +0900

    Add : 물품이 뽑혔을 때, 선택한 물품이 없을 때, 물품을 선택했는데 돈이 부족할 때 로그메시지를 출력하는 함수추가

--- js/LogView.js
+++ js/LogView.js
@@ -7,8 +7,20 @@ class LogView{
         const logMessage = `<p>${coin}원이 투입됐음.</p>`;
         this.logingBox.insertAdjacentHTML("beforeend", logMessage);
     }
-    showNoMoney(coin){
+    showLackYourMoney(coin){
         const logMessage = `<p>넣을 ${coin}원의 갯수가 부족합니다.</p>`;
         this.logingBox.insertAdjacentHTML("beforeend", logMessage);        
+    }
+    showNoItem(){
+        const logMessage = `<p>해당 번호의 물품이 존재하지 않습니다.</p>`
+        this.logingBox.insertAdjacentHTML("beforeend", logMessage);
+    }
+    selectItemLog(itemId, itemName){
+        const logMessage = `<p>${itemId}번 ${itemName}가(이) 뽑혔습니다.</p>`
+        this.logingBox.insertAdjacentHTML("beforeend", logMessage);
+    }
+    showLackInputMoney(){
+        const logMessage = `<p>돈이 부족합니다. 자판기에 돈을 더 넣어주세요.</p>`
+        this.logingBox.insertAdjacentHTML("beforeend", logMessage);
     }
 }
