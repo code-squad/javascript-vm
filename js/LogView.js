f191cbfc76b4f6adb090a615700ad2b15e4baf1e
imdonguk <com8157@naver.com>
2018-08-03 15:15:09 +0900

    Modify : 중복된 일을 하는 코드를 함수로 만들어 함수호출하여 실행하도록함

--- js/LogView.js
+++ js/LogView.js
@@ -1,26 +1,27 @@
 class LogView{
-
-    constructor(){
-        this.logingBox = document.querySelector('.print-action');
-    }
+    
     insertCoinLog(coin){
         const logMessage = `<p>${coin}원이 투입됐음.</p>`;
-        this.logingBox.insertAdjacentHTML("beforeend", logMessage);
+        this.printLogMessage(logMessage);
     }
     showLackYourMoney(coin){
         const logMessage = `<p>넣을 ${coin}원의 갯수가 부족합니다.</p>`;
-        this.logingBox.insertAdjacentHTML("beforeend", logMessage);        
+        this.printLogMessage(logMessage);        
     }
     showNoItem(){
         const logMessage = `<p>해당 번호의 물품이 존재하지 않습니다.</p>`
-        this.logingBox.insertAdjacentHTML("beforeend", logMessage);
+        this.printLogMessage(logMessage);
     }
     selectItemLog(itemId, itemName){
         const logMessage = `<p>${itemId}번 ${itemName}가(이) 뽑혔습니다.</p>`
-        this.logingBox.insertAdjacentHTML("beforeend", logMessage);
+        this.printLogMessage(logMessage);
     }
     showLackInputMoney(){
         const logMessage = `<p>돈이 부족합니다. 자판기에 돈을 더 넣어주세요.</p>`
-        this.logingBox.insertAdjacentHTML("beforeend", logMessage);
+        this.printLogMessage(logMessage);
+    }
+    printLogMessage(logMessage){
+        const logingBox = document.querySelector('.print-action');        
+        logingBox.insertAdjacentHTML("beforeend", logMessage);
     }
 }
