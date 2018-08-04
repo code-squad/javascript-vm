addd29329406942ec92cd5c38ba15cdac713292f
imdonguk <com8157@naver.com>
2018-08-04 13:58:13 +0900

    Add : 돈이 반환되었을 때 동작하는 메서드추가

--- js/MoneyView.js
+++ js/MoneyView.js
@@ -2,7 +2,10 @@ class MoneyView{
     constructor(){
         this.yourMoney = null;
         this.inputMoney = null;
+        this.coinCount = null;
         this.inputMoneyHandler = null;
+        this.returnMoneyHandler = null;
+        this.delayReturnMoneyId = 0;
     }
     walletView(){
         document.querySelector(".your-money").innerText = this.yourMoney + "원";
@@ -10,5 +13,19 @@ class MoneyView{
     inputMoneyView(){
         document.querySelector(".input-money").innerText = this.inputMoney + "원";
         this.inputMoneyHandler();
+    }
+    returnMoney(){
+        const moneyUnit = Object.keys(this.coinCount).sort((a,b) => b-a);
+        let temp = this.inputMoney;
+        this.delayReturnMoneyId = this.delayReturnMoney(moneyUnit,temp);
+    }
+    delayReturnMoney(moneyUnit,temp){
+        return setTimeout(()=>{
+            moneyUnit.forEach(v=>{
+                this.coinCount[v] += Math.floor(temp/v);
+                temp %= v;
+            })
+            this.returnMoneyHandler(this.inputMoney, this.coinCount);
+        },'3000');
     }
 }
