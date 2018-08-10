2b6ee6827212004ba6280ef940d278a7e6524831
imdonguk <com8157@naver.com>
2018-08-10 18:00:47 +0900

    Refactoring : coinCountView와 moneyView가 동작하는게 거의 비슷하여 병합

--- js/MoneyView.js
+++ js/MoneyView.js
@@ -2,7 +2,10 @@ export default class MoneyView{
     constructor(){
         this.inputMoneyHandler = null;
         this.returnMoneyHandler = null;
+        this.insertCoinHandler = null;    
+        this.showNoMoneyHandler = null;
         this.delayReturnMoneyId = 0;
+        this.clickCoinBtns();
     }
     setMoneyData({inputMoney, yourMoney, coinCount}){
         this.inputMoney = inputMoney;
@@ -16,21 +19,36 @@ export default class MoneyView{
         document.querySelector(".input-money").innerText = this.inputMoney + "원";
         this.inputMoneyHandler();
     }
-    returnMoney(){
-        if( this.inputMoney <= 0)return ;
-        const moneyUnit = Object.keys(this.coinCount).sort((a,b) => b-a);
-        let temp = this.inputMoney;
-        this.delayReturnMoneyId = this.delayReturnMoney(moneyUnit,temp);
+    walletCoinView(){
+        let insertCoinBtn = document.querySelectorAll(".insert-coin-button");
+        insertCoinBtn = Array.from(insertCoinBtn);
+        insertCoinBtn.forEach(v=>{
+            v.nextElementSibling.innerHTML = this.coinCount[v.dataset.coin] + "개";
+        })
     }
-    delayReturnMoney(moneyUnit,temp){
-        return setTimeout(()=>{
-            moneyUnit.forEach(v=>{
-                this.coinCount[v] += Math.floor(temp/v);
-                temp %= v;
-            })
-            this.returnMoneyHandler(this.inputMoney, this.coinCount);
-            this.walletView();
-            this.inputMoneyView();
+    moneyView(){
+        this.walletView();
+        this.inputMoneyView();
+        this.walletCoinView();
+    }
+    clickCoinBtns(){
+        const currentCoins = document.querySelector(".wallet > ul");
+        currentCoins.addEventListener("click", ({target})=>{
+            if(!target.getAttribute("data-coin"))return ;
+            this.stopReturnMoney()        
+            const coin = +target.getAttribute("data-coin");
+            if(this.coinCount[coin] === 0){
+                this.showNoMoneyHandler(coin);
+                return ;
+            }
+            this.insertCoinHandler(coin);
+            this.moneyView();
+        })
+    }
+    returnMoney(){
+        this.delayReturnMoneyId = setTimeout(()=>{
+            this.returnMoneyHandler();
+            this.moneyView();
         },'3000');
     }
     stopReturnMoney(){
