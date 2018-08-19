4712ac7aecc89e6cfe44fa85f8dddb627dfc22f7
HYEON(mac) <antaehyeon@live.co.kr>
2018-08-20 02:46:19 +0900

    Feat: util.test.js 기능 완성

--- Test/util.test.js
+++ Test/util.test.js
@@ -2,13 +2,13 @@ import { Utility as util } from '../util.js';
 
 test('숫자 3자리 마다 콤마를 찍습니다', () => {
     // given
-    const num;
+    let num = 440000;
 
     // when
-    num = 440000;
+    const result = util.numberWithCommas(num);
 
     // then
-    expect(util.numberWithCommas(num)).toBe('440,000');
+    expect(result).toBe('440,000');
 });
 
 // test('querySelector 함수가 정상적으로 동작하는지 확인합니다', () => {
@@ -16,71 +16,158 @@ test('숫자 3자리 마다 콤마를 찍습니다', () => {
 //     const content = document.createTextNode('TEST DIV');
 //     node.appendChild(content);
 //
+//     const htmlData = "<body><p>testdata</p></body>"
+//
 //     expect(util.getNodeData('#panel')).toBe(null);
-// })
+// });
 
 test('숫자만 출력하는 정규식을 테스트합니다', () => {
-    // given - when
+    // given
     const testData = 'abcde12345';
 
+    // when
+    const result = util.sortOutNumber(testData);
+
     // then
-    expect(util.sortOutNumber(testData)).toBe(12345);
+    expect(result).toBe(12345);
 });
 
 test('로그를 문장형식으로 만드는지 테스트합니다', () => {
-    // given - when
+    // given
     const testData = 100;
-    const inputModeResult = "100원이 투입됨";
-    const selectModeResult = "100번이 선택됨";
-    const refundModeResult = "100원이 반환됨";
 
-    //then
-    expect(util.addLogSentenceText(testData, 'input')).toBe(inputModeResult);
-    expect(util.addLogSentenceText(testData, 'select')).toBe(selectModeResult);
-    expect(util.addLogSentenceText(testData, 'refund')).toBe(refundModeResult);
+    // when
+    const inputModeResult = util.addLogSentenceText(testData, 'input');
+    const selectModeResult = util.addLogSentenceText(testData, 'select');
+    const refundModeResult = util.addLogSentenceText(testData, 'refund');
+
+    // then
+    expect(inputModeResult).toBe("100원이 투입됨");
+    expect(selectModeResult).toBe("100번이 선택됨");
+    expect(refundModeResult).toBe("100원이 반환됨");
 });
 
 test('모드 문자가 정상적으로 추가되는지 테스트합니다', () => {
-    // given - when
+    // given
     const testData = '테스트';
-    const inputModeResult = "[투입] 테스트";
-    const selectModeResult = "[선택] 테스트";
-    const refundModeResult = "[반환] 테스트";
+    const inputModeResult = util.addLogModeText(testData, 'input');
+    const selectModeResult = util.addLogModeText(testData, 'select');
+    const refundModeResult = util.addLogModeText(testData, 'refund');
 
-    // then
-    expect(util.addLogModeText(testData, 'input')).toBe(inputModeResult);
-    expect(util.addLogModeText(testData, 'select')).toBe(selectModeResult);
-    expect(util.addLogModeText(testData, 'refund')).toBe(refundModeResult);
+    // when - then
+    expect(inputModeResult).toBe("[투입] 테스트");
+    expect(selectModeResult).toBe("[선택] 테스트");
+    expect(refundModeResult).toBe("[반환] 테스트");
 });
 
-// test("노드에 클래스 속성을 설정하는지 테스트합니다", () => {
-//
-//     const node = document.createElement('div');
-//     const content = document.createTextNode('TEST DIV');
-//     node.appendChild(content);
-//
-//     expect(util.setPropertyToItemNode(node, 'high-light')).toBe();
-//
-//     console.log(node);
-// });
+test("노드에 클래스 속성을 설정하는지 테스트합니다", () => {
+    // given
+    const node = document.createElement('div');
+    const content = document.createTextNode('TEST DIV');
 
-// test('nodeList 를 arrayList 로 잘 반환하는지 테스트합니다', () => {
-//
-// });
+    // when
+    node.appendChild(content);
+    util.setPropertyToItemNode(node, 'high-light');
 
+    // then
+    expect(node.classList.contains('high-light')).toBe(true);
+});
 
+test("NodeList 를 array 로 반환하는지 테스트합니다", () => {
+    // given
+    const singleNode = (function () {
+        // make an empty node list to inherit from
+        const nodelist = document.createDocumentFragment().childNodes;
+        // return a function to create object formed as desired
+        return function (node) {
+            return Object.create(nodelist, {
+                '0': {value: node, enumerable: true},
+                'length': {value: 1},
+                'item': {
+                    "value": function (i) {
+                        return this[+i || 0];
+                    },
+                    enumerable: true
+                }
+            }); // return an object pretending to be a NodeList
+        };
+    }());
 
+    const divNode = document.createElement('div');
+    const nodeList = singleNode(divNode); // for example
+    const expectedResult = [];
 
+    // when
+    const receivedData = util.convertNodeListToArray(nodeList);
+    expectedResult.push(divNode);
 
+    // then
+    expect(receivedData).toEqual(expectedResult);
+});
 
+test("상품 선택이 올바른지 테스트합니다", () => {
+    // given
+    const start = 1;
+    const end = 32;
+    const selectiveNum = 5; // 선택된 번호(dummy data)
 
+    // when
+    const receivedData = util.isCorrectSelectedProductNum(selectiveNum, start, end);
 
+    // then
+    expect(receivedData).toBe(true);
+});
 
+test("상품을 구매할 수 있는 상품인지 테스트합니다", () => {
+    // given
+    const price = 2000;
+    const investedMoney = 10000;
 
+    // when
+    const receivedData = util.isPossiblePurchase(price, investedMoney);
 
+    // then
+    expect(receivedData).toBe(true);
+});
 
+test("node의 클래스 속성을 정상적으로 제거하는지 테스트합니다", () => {
+    // given
+    const node = document.createElement('div');
+    const content = document.createTextNode('TEST DIV');
+    node.appendChild(content);
+    util.setPropertyToItemNode(node, 'high-light');
 
+    // when
+    util.removeNodeClass(node, 'high-light');
 
+    // then
+    expect(node.classList.contains('high-light')).toBe(false);
+});
 
+test("에러메세지를 정상적으로 반환하는지 테스트합니다", () => {
+    // given
+    const expectedWalletTestResult = "지갑의 돈이 부족합니다 :(";
+    const expectedMoneyTestResult = "금액이 부족합니다 :(";
+    const expectedProductTestResult = "상품이 존재하지 않습니다 :(";
 
+    // when
+    const walletMode = 'walletMoneyShortage';
+    const moneyMode = 'investedMoneyShortage';
+    const productMode = 'nonExistProduct';
 
+    // then
+    expect(util.getErrorMsg(walletMode)).toBe(expectedWalletTestResult);
+    expect(util.getErrorMsg(moneyMode)).toBe(expectedMoneyTestResult);
+    expect(util.getErrorMsg(productMode)).toBe(expectedProductTestResult);
+});
+
+test("데이터가 0일 경우를 테스트합니다", () => {
+    // given
+    const data = 0;
+
+    // when
+    const expectedResult = util.isMoneyZero(data);
+
+    // then
+    expect(expectedResult).toBe(true);
+});
