07cf1b2bd05542aedd0a35a53b3d4c77a32dd7ff
HYEON(Mac) <antaehyeon@live.co.kr>
2018-10-08 19:33:45 +0900

    Test: Jest, DOM Content, BeforeEach, BeforeAll ...
    1. Jest Test를 진행할 때, Document 객체 추가방법
    2. Jest Test시, querySelector 동작 테스트
    3. Element 출력 테스트
    4. beforeEach, beforeAll 동작 테스트

--- Test/log.test.js
+++ Test/log.test.js
@@ -14,18 +14,76 @@ const logView = new LogView(mainView, util);
  이 파일의 각 테스트가 실행되기 전에 한번만 함수를 실행합니다.
  */
 beforeAll(() => {
+    // console.log("beforeAll LOG");
+    // console.log(document);
+    // console.log(document.innerHTML);
+    // console.log(document.body);
+    // console.log(document.body.classList);
+    console.log("0");
+    console.log(document);
+    console.log(document.body);
     const htmlData = '<div class="status-panel"></div>';
     document.body.insertAdjacentHTML('beforeend', htmlData);
+    console.log("0.1");
+    console.log(document.querySelector('div'));
+    console.log("run beforeAll method");
+    // console.log("beforeAll LOG 2");
+    // console.log(document);
+    // console.log(document.innerHTML);
+    // console.log(document.body);
+    // console.log(document.body.innerHTML);
+    // console.log(document.body.classList);
+
+    const element = document.createElement('div');
+    console.log(JSON.stringify(element, ["id", "className", "tagName"]));
+    element.classList.add('status-panel');
+    console.log(JSON.stringify(element, ["id", "className", "tagName"]));
+    // document.body.insertAdjacentElement('beforeend', element);
+    document.body.appendChild(element);
+
+    console.log("0.2");
+    console.log(document.querySelector('div'));
+    console.log(document.querySelector('.status-panel').classList);
+    console.log(document.body);
+
 });
 
 it ('displayLog 함수가 정상적으로 동작하는지 테스트합니다', () => {
+
+    // test
+    console.log("1");
+    console.log(document.querySelector('#status-panel'));
+    console.log("2");
+    console.log(document.body.querySelector('#status-panel'));
+    console.log("3");
+    console.log(document.querySelector('body'));
+    console.log("4");
+    console.log(document.querySelector('div'));
+
     // given
-    const logData = 10000;
+    let logData = 10000; // 기초 데이터 (테스트 데이터)
     const mode = 'input';
     const result = "[투입] 10000원이 투입됨";
 
-    logView.insertLogDivToLogWindow = jest.fn();
+    // 기존 코드
+    // logView.insertLogDivToLogWindow = jest.fn();
+    /*
+    createLogSentence 를 jest.fn() 으로 대체한 이유
+    1. mainPresenter 에서 getPresenter 가 테스트코드에서는 필요없음
+    동작되려면 복잡함. HTML 코드단에서 동작하는 (Presenter 에 데이터를 보내고.. 등)
+    2. 그런 의미에서 displayLog 의 작업 중 createLogSentence 메서드 동작 중
+    불필요한 작업들이 있음 mainPresenter 의 객체를 받아와서 logPresenter를
+    받아온다거나 등등
+    3. 메서드를 의미를 재 정의하고 하는 역할이 무엇인지 분명하게 정의할 필요가
+    있어보임
+
+    */
     logView.createLogSenetence = jest.fn();
+
+    // createLogSentence 를 대체 (util 에서 2개의 메소드를 호출)
+    logData = util.addLogSentenceText(logData, mode);
+    logData = util.addLogModeText(logData, mode);
+
     // presenter.sendLogDataToModel = jest.fn();
     // logView.createLogSenetence.mockReturnValue(true);
 
@@ -34,10 +92,16 @@ it ('displayLog 함수가 정상적으로 동작하는지 테스트합니다', (
     // logView.displayLog.mockReturnValue(true);
 
     // then
-    expect(logView.createLogSenetence).toHaveBeenCalled();
-    expect(logView.insertLogDivToLogWindow).toHaveBeenCalled();
+    // expect(logView.createLogSenetence).toHaveBeenCalled();
+    // expect(logView.insertLogDivToLogWindow).toHaveBeenCalled();
+    // 181008 debug
+    console.log(document.body);
+    console.log(document.body.innerHTML);
+    console.log(window.document.querySelector('body').innerHTML); //
     // expect(logView.displayLog).toHaveReturnedWith(true);
-    expect(logView.displayLog.name).toEqual("displayLog");
+    // expect(logView.displayLog.name).toEqual("displayLog");
+    // expect(document.querySelector('body').innerHTML).toEqual("displayLog");
+    expect(logView.createLogSentence).toHaveBeenCalled(true);
 });
 
 it('로그 노드(DIV)를 정상적으로 삽입하는지 테스트합니다', () => {
