c2819cd345ee1f1eb15f6c4e894350dedfd530b2
HYEON(Mac) <antaehyeon@live.co.kr>
2018-10-08 19:28:16 +0900

    test: describe 함수 동작 테스트

--- Test/jest.test.js
+++ Test/jest.test.js
@@ -1,11 +1,14 @@
-test('This is a sample', () => {
-  expect(true).toBe(true);
-});
 
-test('there is no I in team', () => {
-  expect('team').not.toMatch(/I/);
-});
+describe("what is describe means?", () => {
+  test('This is a sample', () => {
+    expect(true).toBe(true);
+  });
 
-test('but there is a "stop" in Christoph', () => {
-  expect('Christoph').toMatch(/stop/);
+  test('there is no I in team', () => {
+    expect('team').not.toMatch(/I/);
+  });
+
+  test('but there is a "stop" in Christoph', () => {
+    expect('Christoph').toMatch(/stop/);
+  });
 });
